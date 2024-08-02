e0bc886af27336e097dc7998f1fc78e672ee4e24
karthick raja <85030277+karthick587@users.noreply.github.com>
2024-08-03 01:48:56 +0530

    ui Responsive changes

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,14 +6,10 @@ import RecentOrders from './dashboard/RecentOrdersAndFeedback';
 const Dashboard = () => {
   return (
     <div className="dashboard container-fluid">
-      <div className="row">
-        <div className="col-lg-12">
-          <Cards />
-          <Activity />
-          <RecentOrders />
-        </div>
-      </div>
-    </div>
+      <Cards />
+      <Activity />
+      <RecentOrders />
+    </div >
   );
 };
 
